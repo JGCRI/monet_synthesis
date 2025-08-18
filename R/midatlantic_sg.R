c07850564604f4c4e8e2a71258b938dc43f75c61
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2025-08-18 16:29:37 -0400

    Update midatlantic_sg.R

--- R/midatlantic_sg.R
+++ R/midatlantic_sg.R
@@ -4,6 +4,21 @@ library(sf)
 library(tidyterra)
 library(ggplot2)
 
+# state_abbreviations_conus <- c(
+#   "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "ID", "IL", "IN", "IA", "KS", "KY",
+#   "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
+#   "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA",
+#   "WV", "WI", "WY"
+# )
+
+# conus_valid <- st_read("data/shapefiles/s_18mr25/s_18mr25.shp") %>%
+#   st_make_valid() %>%
+#   filter(STATE %in% state_abbreviations_conus) %>%
+#   st_transform("WGS84")
+
+# climate_zones_sf <- st_read("data/shapefiles/na_climatezones_shapefile/climatezones_shapefile/NA_ClimateZones/data/North_America_Climate_Zones.shp")%>%
+#   st_transform(crs(conus_valid))
+
 # Step 1: Load and crop raster
 sg_clay_full <- terra::rast("./data/soilgrids/crop_roi_igh_clay_0-5cm.tif") # Load full raster
 crop_extent <- terra::ext(-78, -74.5, 38.25, 40.5) # Define crop extent
@@ -12,21 +27,10 @@ sg_clay_top_prj <- sg_clay_full %>%
 sg_clay_top_prj_final <- terra::crop(sg_clay_top_prj, crop_extent)  # Crop raster to extent
 
 # Step 2: Reproject to match vector layer CRS
-target_crs <- sf::st_crs(conus_valid)$wkt                # Extract CRS from `sf` object
+target_crs <- sf::st_crs(conus_valid)$wkt      # Extract CRS from `sf` object
 sg_clay_top_prj <- terra::project(sg_clay_top_prj_final, target_crs) # Reproject raster
 
 # Step 3: Apply transformation
 sg_clay_top_prj <- sg_clay_top_prj / 10  # Scale raster values as needed
 
-#terra::writeRaster(sg_clay_top_prj, "sg_clay_top_prj.tif", overwrite = TRUE)
-
-# Step 4: Plot with ggplot
-ggplot() +
-  tidyterra::geom_spatraster(data = sg_clay_top_prj) +
-  geom_sf(data = conus_valid, color = "grey", alpha = 0.3) +
-  geom_sf(data = top_clay_test, color = "black", size = 4) +
-  geom_sf(data = top_clay_test, aes(color = Site_Code), size = 4) +
-  coord_sf(xlim = c(-78, -74.5), ylim = c(38.25, 40.5)) +
-  theme_bw() +
-  scale_fill_gradient(low = "white", high = "blue", na.value = NA) +
-  labs(title = "MONet data points without Soilgrids data")
+terra::writeRaster(sg_clay_top_prj, "sg_clay_top_prj.tif", overwrite = TRUE)
